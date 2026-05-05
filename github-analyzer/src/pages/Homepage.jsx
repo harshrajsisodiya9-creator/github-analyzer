cdf8eee373b7051358220c6c0a8fdbb07a7dbbc0
harshrajsisodiya9-creator <harshrajsisodiya9@gmail.com>
2026-05-05 22:23:18 +0530

    error handling done

--- github-analyzer/src/pages/Homepage.jsx
+++ github-analyzer/src/pages/Homepage.jsx
@@ -8,33 +8,55 @@ export default function Homepage(){
 
     const [username, setUsername] = useState("");
     const [result, setResult] = useState([]);
+    const [loading,setLoading] = useState(false);
+    const [error, setError] = useState(null);
     
-    const handleSearch = async() => {
+    const handleSearch = async(signal) => {
         try{
-            const res = await axios.get(`https://api.github.com/search/users?q=${username}`);
+            setLoading(true);
+            const res = await axios.get(`https://api.github.com/search/users?q=${username}`, {signal});
             setResult(res.data.items);
         }
         catch(e){
-            console.error(e);
+            if (e.name === "CanceledError") return;
+
+            let message = "Something went wrong";
+
+            if (e.response && e.response.data) {
+            message = e.response.data.message || message;
+            } else if (e.message) {
+            message = e.message;
+            }
+
+            setError(message);
+        }
+        finally{
+            setLoading(false);
         }
     }
 
 
     // debouncing(limting api calls)
     useEffect(() => {
-
+        const controller = new AbortController();
         const timer = setTimeout(() =>{
-            if(username){handleSearch();}
+            if(username){handleSearch(controller.signal);}
             else setResult([]);
         },500)
 
-        return () => clearTimeout(timer);
+        return () => {clearTimeout(timer)
+            controller.abort();
+        };
 
     },[username])
 
     return (
         <div className="container">
             <SearchBar value = {username} onChange = {setUsername} onSearch = {handleSearch}/>
+            <div>
+                {loading && <h3>Loading ....</h3>}
+                {error && <h3>{error}</h3>}
+            </div>
             {result.length > 0 && (
                 <div>
                     <UserGrid users = {result} />
